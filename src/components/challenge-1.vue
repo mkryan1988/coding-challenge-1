31343114a212929f68e307400e13f1320b6c9ab2
Mark <mkryan1988@gmail.com>
2019-04-30 17:01:06 +0100

    Progress on method layout plus ship roatation functions added

--- src/components/challenge-1.vue
+++ src/components/challenge-1.vue
@@ -5,17 +5,94 @@
 
 <script>
 export default {
+  /* eslint-disable */
   name: 'oceanGrid',
   data () {
     return {
-      windowWidth: '',
-      transitionName: 'fade',
-      enter: null,
-      leave: null
+      ships: [
+        [ '1 1 E', 'RFRFRFRF']
+        // [ '3 2 N', 'FRRFLLFFRRFLL'],
+        // [ '0 3 W', 'LLFFFLFLFL']
+      ],
+      gridNE: '5 3',
+      output: [],
     }
   },
   mounted () {
+    this.locateShips()
   },
+  methods: {
+    locateShips () {
+      for (var ship of this.ships) {
+        this.routeShip(ship)
+      }
+    },
+    routeShip(ship) {
+      var self = this
+      const startingPos = ship[0]
+      const instructions = ship[1]
+      var newPos = startingPos
+      console.log('pos:' + startingPos)
+      for (let inst of instructions) {
+        if (inst === 'L') {
+          setTimeout(function() {
+            newPos = self.rotateLeft(newPos)
+          },500);
+        } else if (inst === 'R') {
+            setTimeout(function() {
+              newPos = self.rotateRight(newPos)
+            },500);
+        } else if (inst === 'F') {
+          setTimeout(function() {
+            newPos = self.moveForward(newPos)
+          },500);
+        }
+      }
+      console.log('pos:' + newPos)
+    },
+    rotateLeft (newPos) { 
+      var oldDirection = newPos[newPos.length -1];
+      var newdirection = ''
+      if (oldDirection === 'N') {
+        newdirection = 'W'
+      } else if (oldDirection === 'E') {
+        newdirection = 'N'
+      } else if (oldDirection === 'S') {
+        newdirection = 'E'
+      } else if (oldDirection === 'W') {
+        newdirection = 'S'
+      }
+      return newPos.replace(/.$/, newdirection)
+    },
+    rotateRight (newPos) {
+      var oldDirection = newPos[newPos.length -1];
+      var newdirection = ''
+      if (oldDirection === 'N') {
+        newdirection = 'E'
+      } else if (oldDirection === 'E') {
+        newdirection = 'S'
+      } else if (oldDirection === 'S') {
+        newdirection = 'W'
+      } else if (oldDirection === 'W') {
+        newdirection = 'N'
+      }
+      return newPos.replace(/.$/, newdirection)
+    },
+    moveForward (newPos) {
+      // if is past grid coordinates
+      if (condition) {
+        
+      } else {
+        return 
+      }
+      return newPos.replace(/.$/, newdirection)
+    }
+  },
+  computed: {
+    ifLost () {
+      return 'LOST'
+    }
+  }
 }
 </script>
 
